7a60ad8fd85e4916c1a458d695fda8c9561fcac6
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-08-03 02:40:28 +0300

    add a line

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ class Forecast {
 }
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(` Server listening on port ${PORT}`);
 });
