468b0eb507296d26db4ea61bcbd693ffb1745c24
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-08-06 20:13:59 +0300

    solve problem

--- modules/weatherModule.js
+++ modules/weatherModule.js
@@ -5,17 +5,16 @@ const handlerError = require('../server')
 
 async function handlerWeather (req, res) {
     
-    const searchQuery = req.query.searchQuery;
     const lat =req.query.lat;
     const lon =req.query.lon;
-  
+   console.log(lat,lon)
     
     //const cityArr = weatherData.find(item => item.city_name.toLowerCase() === searchQuery.toLowerCase());
-    const cityArr = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`)
-   
-  
+    const url = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`)
+   console.log(url);
     try {
-      const cityData = cityArr.data.data.map ((item) => new Forecast(item));
+      const cityData = url.data.data.map ((item) => new Forecast(item));
+      console.log(cityData)
       //we use (res) to having the data back from respone
       res.status(200).send(cityData);
     } catch (error) {
